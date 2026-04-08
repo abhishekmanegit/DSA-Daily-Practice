d6e9a32ffde14eb3ddf5bee21c1562acc23040ab
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-04-08 22:52:18 +0530

    Max unit on a truck

---
+++ arrays/MaxUnit.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+
+        Arrays.sort(boxTypes, (a,b) -> Integer.compare(b[1],a[1]));
+
+        int units = 0;
+
+        for(int[] boxes:boxTypes){
+
+            int numberofBoxes = boxes[0];
+            int unitperBoxes = boxes[1];
+
+            if(truckSize >= numberofBoxes){
+
+                units += numberofBoxes * unitperBoxes;
+                truckSize -= numberofBoxes;
+            }
+            else{
+
+                units += truckSize * unitperBoxes;
+                return units;
+            }
+        }
+
+        return units;
+        
+    }
+}
