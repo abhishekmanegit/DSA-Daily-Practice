261d00e8743b2ba2a68ead41f3aa1ee36ac8b297
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-04-20 22:29:01 +0530

    Some Chnages Are Made

--- JavaPractice/AddTwoNumbers.java
+++ JavaPractice/AddTwoNumbers.java
@@ -7,6 +7,8 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+ * 
+ * this is easy solution
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
